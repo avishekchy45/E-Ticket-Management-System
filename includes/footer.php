71bbd5784d6bd0aec944be713e3bc7cd43b69d26
IftekherM <mahmudifte@gmail.com>
2021-04-05 23:17:29 +0600

    Footer Added

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+<nav class='navbar navbar-expand-sm bg-info navbar-dark sticky-top'>    
+        <ul class='navbar-nav'>
+            <li class='nav-item'>
+                <a class='nav-link' href='about.php'> About US </a>
+            </li>
+            <li class='nav-item'>
+                <a class='nav-link' href='contact.php'> Contact US </a>
+            </li>
+        </ul>
+</nav>
+<br>
+
