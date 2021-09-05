45235c8036158c79bf5164d02350867de50401ca
avishekchy45 <avishekchy45@gmail.com>
2021-09-05 19:12:13 +0600

    Workspace File Added

--- login.php
+++ login.php
@@ -8,7 +8,7 @@ include("includes/isloggedin.php");
 <html lang="en">
 
 <head>
-<meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
@@ -16,7 +16,7 @@ include("includes/isloggedin.php");
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <link rel="stylesheet" href="style.css">
     <link rel="icon" type="image/png" href="logo.png" />
     <title>LOGIN</title>
@@ -46,7 +46,7 @@ include("includes/isloggedin.php");
         <div class="row">
             <!-- MENU -->
             <div class="col-sm-2 text-left">
-                
+
             </div>
             <!-- MAIN -->
             <div class="col-sm-8 text-center">
